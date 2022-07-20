1247b3d2466f6538e6e2018c70150a0f4983284a
Nicholas <nicholasjoven@gmail.com>
2022-07-20 14:04:26 -0400

    updated background colors dynamically

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import './App.css';
 import Fighter from './components/Fighter'
 
 function App() {
-  const fighters = ['Roy', 'Ganondorf', 'Mario', 'Luigi', 'Marth', 'Link', 'Inkling', 'Samus', 'Fox', 'Sheik', 'Zelda', 'Ken']
+  // started here const fighters = ['Roy', 'Ganondorf', 'Mario', 'Luigi', 'Marth', 'Link', 'Inkling', 'Samus', 'Fox', 'Sheik', 'Zelda', 'Ken']
+  // goal is to have array of objects
+  const fighters = [
+    {name: 'Roy', color: 'blue'},
+    { name: 'Ike', color: 'brown'},
+    { name: 'Lucina', color: 'lightskyblue'},
+    {name: 'Marth', color: 'dodgerblue'},
+    {name: 'Byleth', color: 'gray'},
+    {name: 'Chrom', color: 'orange'},
+    {name: 'Corrin', color: 'gold'},
+    {name: 'Sheik', color: 'coral'},
+    { name: 'Zelda', color: 'pink'},
+    {name: 'Sephiroth', color: 'black'},
+    {name: 'Cloud', color: 'midnightblue'},
+    {name: 'Sora', color: 'firebrick'},
+  ]
+
   return (
     <div className="App">
       <h1>Fighters</h1>
--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Fighter = ({fighter}) => {
     return (
-        <div>
-            <img className='fighter-img' src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.toLowerCase()}.png`} />
-            <h5 className='fighter-name'>{fighter}</h5>
+        <div style={{backgroundColor: fighter.color}}>
+            <img className='fighter-img' src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.name.toLowerCase()}.png`} />
+            <h5 className='fighter-name'>{fighter.name}</h5>
         </div>
     )
 }
