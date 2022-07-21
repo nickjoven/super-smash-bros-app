7037c2bd917c7b6beb98f36e43653487177bf947
Nicholas <nicholasjoven@gmail.com>
2022-07-21 15:52:40 -0400

    started fighter screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from 'react'
 import './App.css';
 import Fighter from './components/Fighter'
+import FighterScreen from './components/FighterScreen'
+
 
 function App() {
   // started here const fighters = ['Roy', 'Ganondorf', 'Mario', 'Luigi', 'Marth', 'Link', 'Inkling', 'Samus', 'Fox', 'Sheik', 'Zelda', 'Ken']
   // goal is to have array of objects
   const fighters = [
     {name: 'Roy', color: 'blue'},
-    { name: 'Ike', color: 'brown'},
+    { name: 'Chrom', color: 'white'},
     { name: 'Lucina', color: 'lightskyblue'},
     {name: 'Marth', color: 'dodgerblue'},
     {name: 'Byleth', color: 'gray'},
-    {name: 'Chrom', color: 'orange'},
+    {name: 'Ike', color: 'brown'},
     {name: 'Corrin', color: 'gold'},
     {name: 'Sheik', color: 'coral'},
     { name: 'Zelda', color: 'pink'},
@@ -19,6 +22,8 @@ function App() {
     {name: 'Sora', color: 'firebrick'},
   ]
 
+  const [selectedFighter, setSelectedFighter] = useState()
+
   return (
     <div className="App">
       <h1>Fighters</h1>
@@ -26,11 +31,16 @@ function App() {
         {
           fighters.map((element, index) => {
             return (
-              <Fighter fighter={element} />
-            )
-          })
-        }
+              <Fighter fighter={element} setSelectedFighter={setSelectedFighter} />
+              )
+            })
+          }
       </div>
+    {
+      selectedFighter
+      ? <FighterScreen />
+      : null
+    }
     </div>
   );
 }
--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Fighter = ({fighter}) => {
+const Fighter = ({ fighter, setSelectedFighter }) => {
     return (
-        <div style={{backgroundColor: fighter.color}}>
+        <div style={{backgroundColor: fighter.color}} onClick={() => setSelectedFighter(fighter.name)}>
             <img className='fighter-img' src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.name.toLowerCase()}.png`} />
             <h5 className='fighter-name'>{fighter.name}</h5>
         </div>
